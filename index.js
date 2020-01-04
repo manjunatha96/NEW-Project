2547c73f9ac4864b48ed1f3544f6297b0f418931
Manjunatha <58422091+manjunatha96@users.noreply.github.com>
2020-01-04 14:25:19 +0530

    fiv commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('winston-mongodb')
 const express= require('express')
 const bodyParser=require('body-parser')
 const winston=require('winston')
+const cors=require('cors')
 const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(cors())
 require('./Middleware/logs')();
 require('./router/routes')(app)
 require('./Shared/db')();
